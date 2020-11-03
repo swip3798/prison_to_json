2ab5b95bb4fad82e719182f1a54829c33f2fc846
Schweigel <christian.david.schweigel@sap.com>
2020-11-03 17:07:27 +0100

    Remove dead code

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,6 @@ extern crate clap;
 use clap::{App, Arg}; 
 use json;
 
-#[allow(dead_code)]
-fn prison_to_json() -> json::JsonValue {
-    let reader = filereader::FileReader::new(String::from("example.prison"));
-    let raw_prison = reader.to_string_with_default(String::from("lol"));
-    let tokens = lexer::tokenize(raw_prison);
-    let mut parser = parser::Parser::new(tokens);
-    let prison_obj = parser.get_json_value();
-    let writer = filewriter::FileWriter::new(String::from("output.json"));
-    writer.write_or_update(prison_obj.pretty(2));
-    return prison_obj;
-}
-
-#[allow(dead_code)]
-fn json_to_prison(obj: json::JsonValue) {
-    let mut gen = generator::Generator::new(obj);
-    let tokens = gen.generate_prison().unwrap();
-    let writer = filewriter::FileWriter::new(String::from("output.prison"));
-    writer.write_or_update(tokens.join(""));
-}
-
 fn main() {
     let matches = App::new("Prison Architect savefile to json Parser")
        .version("1.0")
